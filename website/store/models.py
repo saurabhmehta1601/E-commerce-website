31b3e3b851e77e121021ab0972421c9c0363a056
saurabhmehta1601 <saurabhmehta1601@gmail.com>
2021-01-13 22:52:04 +0530

    cart and checkout page front design done

--- website/store/models.py
+++ website/store/models.py
@@ -20,6 +20,15 @@ class Product(models.Model):
     def __str__(self) :
         return self.name
 
+    @property
+    def imageURL(self):
+        try :
+            url=self.image.url
+        except :
+            url="../static/images/nopreview.svg"
+        return url 
+        
+
 class Order(models.Model):
     customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
     date_ordered=models.DateTimeField(auto_now_add=True)
